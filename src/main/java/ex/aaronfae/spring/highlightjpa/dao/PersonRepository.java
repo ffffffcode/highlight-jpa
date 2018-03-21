7754364825df890bed2993b350b452a33b9b6ad9
AaronFae <AaronFae@163.com>
2018-03-21 19:22:54 +0800

    spring-boot-starter-rest

--- src/main/java/ex/aaronfae/spring/highlightjpa/dao/PersonRepository.java
+++ src/main/java/ex/aaronfae/spring/highlightjpa/dao/PersonRepository.java
@@ -3,8 +3,13 @@ package ex.aaronfae.spring.highlightjpa.dao;
 
 import ex.aaronfae.spring.highlightjpa.entity.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
+@RepositoryRestResource(path = "people")
 public interface PersonRepository extends JpaRepository<Person, Long> {
 
-    Person findByNameStartsWith(String name);
+    @RestResource(path = "nameStartsWith", rel = "nameStartsWith")
+    Person findByNameStartsWith(@Param("name") String name);
 }
